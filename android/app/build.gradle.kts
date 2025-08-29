2e44984a0aeb5e47b2d1278220f5ab33776da703
santanaoliva-u <santanaoliva-u@github.com>
2025-08-29 15:40:07 -0500

    Fix: Regenerar estructura Android y actualizar versiones de Gradle para compatibilidad

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.webboostmarketing.salon_app"
+    namespace = "com.example.temp_app"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.webboostmarketing.salon_app"
+        applicationId = "com.example.temp_app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
